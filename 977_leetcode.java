7e8b369fc8503370fb2824a9ec446ff99d3dc60b
Gungun Saluja <gungunsaluja16@gmail.com>
2024-03-02 10:12:08 +0530

    Update 977_leetcode.java

--- 977_leetcode.java
+++ 977_leetcode.java
@@ -1,1 +1,9 @@
 
+977. Squares of a Sorted Array
+  publis int[] sortedSquares(int nums[]){
+  for(int i=0i<nums.length;i++){
+    nums[i] = nums[i]*nums[i];
+  }
+  Arrays.sort(nums);
+  return nums;}
+  
