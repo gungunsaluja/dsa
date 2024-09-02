b5f63eaac75986645ae7685ed3aa757a0cdb7b9f
Gungun Saluja <gungunsaluja16@gmail.com>
2024-09-03 00:02:58 +0530

    Update Rotate LinkedList.java
    // The main intuition of the question is that first we need to calculate the length of the linked list so that we can reach
    // to its tail node after doing this we need to check that weather k%len==0 and if this condition is true then we dont have do any
    // thing because because after rotating k times we will get the same linked list so we will return the same head for this condition
    //  after words we have to calculate len -k && k should be k = k%len so that it can handle large values
    // then resultant node will point to the null and the next node of it will be head

--- Rotate LinkedList.java
+++ Rotate LinkedList.java
@@ -1,3 +1,9 @@
+// The main intuition of the question is that first we need to calculate the length of the linked list so that we can reach
+// to its tail node after doing this we need to check that weather k%len==0 and if this condition is true then we dont have do any
+// thing because because after rotating k times we will get the same linked list so we will return the same head for this condition
+//  after words we have to calculate len -k && k should be k = k%len so that it can handle large values
+// then resultant node will point to the null and the next node of it will be head
+
 class Solution {
 
    
